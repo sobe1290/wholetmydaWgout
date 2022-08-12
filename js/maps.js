d0e499f580534f111d284f849dbb0f16e6791eb6
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-08-11 21:12:38 -0700

    adding maps script tag

--- js/maps.js
+++ js/maps.js
@@ -1,1 +1,7 @@
-// removed old map js, going to make custom calls.. i hope so.
+// Putting in notes here before I write the JS.
+
+
+// Maps Static API
+// "https://maps.googleapis.com/maps/api/staticmap?center=" + Search Var + "&zoom=14&size=400x400&key=" + YOUR_API_KEY +"&signature=YOUR_SIGNATURE"
+
+
