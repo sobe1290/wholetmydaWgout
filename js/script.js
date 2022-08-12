3f011bd6ee5da18821482aed30383f992cc7a370
zsheperdigian <zsheperdigian@gmail.com>
2022-08-11 21:16:10 -0700

    added more of the nav bar including an input for loction, input for distance radius, drop down for options

--- js/script.js
+++ js/script.js
@@ -8,4 +8,4 @@
     
 //create function to grab click to submit form
     // -prevent default for form
-    // -set location to a variable and then send it to needed function.
+    // -set location to a variable and then send it to needed function
