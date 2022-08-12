2c65df216f493ce51250d6986274fde1206c5324
zsheperdigian <zsheperdigian@gmail.com>
2022-08-11 21:03:00 -0700

    added sudo code in js to help submit form

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Shep will write stuff here...
+
+//To Do: 
+
+
+
+
+    
+//create function to grab click to submit form
+    // -prevent default for form
+    // -set location to a variable and then send it to needed function.
