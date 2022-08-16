291b6b60f29c92c2d51442f8454fd6019b0213bb
Akarim Abdi <108248869+K2YFam@users.noreply.github.com>
2022-08-15 19:24:50 -0700

    using AJAX to get and display weather data in console

--- js/OpenWeather.js
+++ js/OpenWeather.js
@@ -1,2 +1,15 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid={API key}')
+const apiKey = "&appid=9f103066ad2690dfc98026104a1b9e25"
+const mainDate = moment().format("MMM Do, YYYY");
 
+const cityName = 'Seattle'
+const URL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + apiKey
+const queryURLforcast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=ecc0be5fd92206da3aa90cc41c13ca56";
+$.ajax({
+    url: URL,
+    method: "GET" 
+
+}).then(function(response){
+    console.log(response);
+    console.log(URL);
+
+})
