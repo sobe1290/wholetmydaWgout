4ab64fb6867d9ea67e82bdb441e281c20f801d04
zsheperdigian <zsheperdigian@gmail.com>
2022-08-16 20:06:49 -0700

    added DOM map elements

--- js/script.js
+++ js/script.js
@@ -1,18 +1,33 @@
 // Shep will write stuff here...
 
-//To Do: 
-
+//create variables for DOM mapping
+var searchForm = document.querySelector(".navBar"); //search form
+var userLocation = document.querySelector("#userLocat"); // users Location
+var submitbtn = document.querySelector("#submitbtn");
 
 
 
     
 //create function to grab click to submit form
+submitbtn.addEventListener(initMap());
     // -prevent default for form
-    // -set location to a variable and then send it to needed function
+    // -set location to a variable and then send it to needed function ***grab from Karim
 
 
 //create function to record user choices
+    //grab the user input for location
+    //grab the checked boxes
 
 
 
 //create function to send user choices to correct spot(create variable to go to)
+
+
+/// **** stuff from Patrick************
+
+// var searchTerm = 'Veterinarian' //Shep change this
+// var selectedRadius = '500' //Shep change this
+// var lat = 47.60 // comes from Karim
+// var long = -122.33 // comes from Karim
+// //make function to run initMap
+//***********************************
