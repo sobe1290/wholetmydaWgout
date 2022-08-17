78a52facad132c0c567b14ef917bd11f52b19bd1
Scott-Gauthier <gauthier.scott@gmail.com>
2022-08-16 21:04:22 -0700

    storing localstorage to github

--- js/script.js
+++ js/script.js
@@ -9,5 +9,20 @@
 //create function to grab click to submit form
     // -prevent default for form
     // -set location to a variable and then send it to needed function
+//local storage variables
+var citylistEL = $('.citylist');
+var textEntered = localStorage.getItem("textEntered") || [];
+
+for (let i = 0; i < textEntered.length; i++) {
+    var locationentered = JSON.parse(localStorage.getItem("textEntered")) || "";
+    if (!locationentered[i]) {
+        locationentered[i] = ""
+    } else {
+        locationentered[i] = locationentered[i]
+    };
+    //Create list element
+    timeblockEL.append($(`<li id="li${i}">${locationentered[i] || []}</li>`));
 
 
+
+}
