25587a8bcec0ec62b870813aa44821abd8b31f82
zsheperdigian <zsheperdigian@gmail.com>
2022-08-16 18:56:32 -0700

    added more detail to check boxes and added sudo code for function

--- js/script.js
+++ js/script.js
@@ -11,3 +11,8 @@
     // -set location to a variable and then send it to needed function
 
 
+//create function to record user choices
+
+
+
+//create function to send user choices to correct spot(create variable to go to)
