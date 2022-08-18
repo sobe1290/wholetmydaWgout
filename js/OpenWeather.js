55c26f6033259ace536c843a93d1f63318f1e2eb
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-08-17 22:01:55 -0700

    changes the target value of form to match idex

--- js/OpenWeather.js
+++ js/OpenWeather.js
@@ -3,8 +3,8 @@ const apiKey = "&appid=9f103066ad2690dfc98026104a1b9e25"
 const mainDate = moment().format("MMM Do, YYYY");
 
 $("#cityInputSubmit").on("click", () => {
-    const cityName = $("#password").val();
-    $("#password").val("");
+    const cityName = $("#cityInput").val();
+    $("#cityInput").val("");
     const URL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + apiKey
     const queryURLforcast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=ecc0be5fd92206da3aa90cc41c13ca56";
     $.ajax({
