81c4a847f0ffd03c4a59580e57f320c0d808eaf2
Akarim Abdi <108248869+K2YFam@users.noreply.github.com>
2022-08-18 20:16:06 -0700

    added icons to temp and weather
    added icons to temp

--- js/OpenWeather.js
+++ js/OpenWeather.js
@@ -31,7 +31,7 @@ $("#cityInputSubmit").on("click", () => {
     console.log(searchTerm)
     const cityName = $("#cityInput").val();
     $("#cityInput").val("");
-    const URL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + apiKey
+    const URL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + apiKey+ "&units=imperial"
     const queryURLforcast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=ecc0be5fd92206da3aa90cc41c13ca56";
     $.ajax({
         url: URL,
@@ -42,19 +42,81 @@ $("#cityInputSubmit").on("click", () => {
     console.log(response.weather)
     console.log(response.coord);
     console.log(URL);
-    var lat = response.coord.lat;
-    var long = response.coord.lon;
-    var weather =response.weather[0].main;
-    var temp = response.main.temp;
-    console.log('Latitutde:', lat)
-    console.log('Longitude:' , long)
-    console.log('Weather:' , weather)
+    var cityNameEl = $("<h2>").text(response.name);
+    var displayMainDate = cityNameEl.append(" " + mainDate);
+    var tempEL = $("<p>").text("Temperture: " + response.main.temp);
+    var humEl = $("<p>").text("Humidity: " + response.main.humidity);
+    var windEl = $("<p>").text("Wind Speed: " + response.wind.speed);
+    var currentweather = response.weather[0].main;
 
-    var container3 =$("<h1>")
-    var container4 =$("<h1>")
-    container3.text(weather)
-    container4.text(temp)
-    $("#testVar").append(container3,container4)
+    if (currentweather === "Rain") {
+        var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/09d.png");
+        currentIcon.attr("style", "height: 60px; width: 60px");
+    } else if (currentweather=== "Clouds") {
+        var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/03d.png");
+        currentIcon.attr("style", "height: 60px; width: 60px");
+    } else if (currentweather === "Clear") {
+        var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/01d.png");
+        currentIcon.attr("style", "height: 60px; width: 60px");
+    }
+     else if (currentweather === "Drizzle") {
+        var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/10d.png");
+        currentIcon.attr("style", "height: 60px; width: 60px");
+    }
+     else if (currentweather === "Snow") {
+        var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/13d.png");
+        currentIcon.attr("style", "height: 60px; width: 60px");
+    }
+    //appending to render on page
+    var newDiv = $('<div>');
+
+    newDiv.append(displayMainDate, currentIcon, tempEL, humEl, windEl);
+
+    $("#testVar").html(newDiv);
+
+    // var lat = response.coord.lat;
+    // var long = response.coord.lon;
+    // var weather =response.weather[0].main;
+    // var temp = response.main.temp;
+    // console.log('Latitutde:', lat)
+    // console.log('Longitude:' , long)
+    // console.log('Weather:' , weather)
+
+    // var container3 =$("<h1>")
+    // var container4 =$("<h1>")
+    // var icon =$('01D')
+    // container3.text(weather)
+    // container4.text(temp)
+    // $("#testVar").append(container3,container4)
+    
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const { main, name, sys, weather } = data;
+      const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
+        weather[0]["icon"]
+      }.svg`;
+
+      const li = document.createElement("li");
+      li.classList.add("city");
+      const markup = `
+        <h2 class="city-name" data-name="${name},${sys.weather}">
+          <span>${name}</span>
+          <sup>${sys.Weather}</sup>
+        </h2>
+        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+        <figure>
+          <img class="city-icon" src="${icon}" alt="${
+        weather[0]["description"]
+      }">
+          <figcaption>${weather[0]["description"]}</figcaption>
+        </figure>
+      `;
+      li.innerHTML = markup;
+      list.appendChild(li);
+    })
     
 
 
